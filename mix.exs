58df85b919753f674171351401f58a96bfeb63f5
Adriano Mota <adrianowsh@hotmail.com>
2021-04-02 19:06:11 -0300

    add credo for quality code

--- mix.exs
+++ mix.exs
@@ -1,4 +1,7 @@
 defmodule FindMyPersonal.MixProject do
+  @moduledoc """
+    Very useful module
+  """
   use Mix.Project
 
   def project do
@@ -44,7 +47,8 @@ defmodule FindMyPersonal.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
